aa0b6f289e89c4b025af9e9fef00dc057dd10a09
Daniel <danielmorillo249@hotmail.com>
2017-05-06 03:34:39 +0200

    added lancer

--- types.cs
+++ types.cs
@@ -44,4 +44,22 @@ class Archer : Unit {
         
         Printer.printSpawn(this);
     }
+    class Lancer : Unit {
+        public Lancer(bool isPl) {
+            isPlayer = isPl;
+            idleCode = "+";
+            hitCode = "+";
+            attackDamage = 19;
+            attackRange = 1;
+            aoeRange = 1;
+            atackSpeed = 1;
+            moveRange = 1;
+            healthPoints = 90;
+            blockDmg = 10;
+            tipo = "lancer";
+
+            hash = hashCounter;
+            hashCounter++;
+        }
+    }
 }
