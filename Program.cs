cd1f28f55cb06d219ce8fa52c6f707325bd42c53
Daniel <danielmorillo249@hotmail.com>
2017-10-15 02:36:18 +0200

    added console class set up methods calls

--- Program.cs
+++ Program.cs
@@ -4,14 +4,16 @@ class Program {
     static void Main(string[] args) {
         Console.WriteLine("Hello World!");
         Terminal termin = new Terminal();
-        termin.ResetBuffer();
+        Console.Clear();
+        //termin.ResetBuffer(30, 60);
+
         // tost();
         // battle constructor(int turnDuration, int mapSize)
         Battle battle = new Battle(50, 100);
         // test(battle, 5, true);
         // test(battle, 5, false);
         // run(int armySizeForEachPlayer)
-        battle.run(0);
+        battle.run();
     }
 
 }
--- console.cs
+++ console.cs
@@ -3,6 +3,23 @@ using System.Diagnostics;
 using System.IO;
 public class Terminal {
     Process OutputConsole;
+    // rescale whole console
+    public void ResetBuffer(int height, int width) {
+        Console.WindowHeight = height;
+        Console.BufferHeight = height;
+        Console.WindowWidth = width;
+        Console.BufferWidth = width;
+    }
+    public void PrintSym(string symbol, int pos_x, int pos_y, ConsoleColor color) {
+        Console.SetCursorPosition(pos_x,pos_y);
+        Console.ForegroundColor = color;
+        Console.Write(symbol);
+    }
+    public void PrintText(string text, int pos_x, int pos_y, ConsoleColor color) {
+        Console.SetCursorPosition(pos_x,pos_y);
+        Console.ForegroundColor = color;
+        Console.Write(text);
+    }
     public void exitConsole() {
         //OutputConsole.
     }
